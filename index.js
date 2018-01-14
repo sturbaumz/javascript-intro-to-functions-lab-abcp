5189aa88a69f2a4c00bc2f816b423dd6e8d72271
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-14 04:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function logShout(string) { console.log(string.toUpperCase())
 function logWhisper(string) {console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string)
-{if (sayHiToGrandma(string.toLowerCase)===sayHiToGrandma(string))
+{if (string.toLowerCase()===string)
   {return "I can't hear you!"
 
-} else if (sayHiToGrandma(string.toUpperCase)===sayHiToGrandma(string))
+} else if (sayHiToGrandma(string.toUpperCase())===sayHiToGrandma(string))
 {return "YES INDEED!"
 
 } else if (sayHiToGrandma(string)===("I love you, Grandma.")){return "I love you, too."}}
